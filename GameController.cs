55d1cd02d023467cea7a8b5a37cb0875b2ee5d0c
alisonmunnis <46458925+alisonmunnis@users.noreply.github.com>
2019-04-22 18:09:03 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public AudioSource winMusic;
+    public AudioSource loseMusic;
+
+    public Text ScoreText;
+    public Text RestartText;
+    public Text GameOverText;
+    public Text winText;
+   
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+    [SerializeField] private Text uitext;
+    [SerializeField] private float mainTimer;
+
+    void Start ()
+    {
+        timer = mainTimer;
+        gameOver = false;
+        restart = false;
+        RestartText.text = "";
+        GameOverText.text = "";
+        winText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+
+    }
+
+   void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uitext.text = timer.ToString("F");
+        }
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uitext.text = "0.00";
+            timer = 0.0f;
+            gameOver = true;
+
+        }
+
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                SceneManager.LoadScene("Munnis_Alison_DIG3480_Challenge3");
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        { 
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            { 
+                //GetComponent<AudioSource>().Stop();
+                //loseMusic.Play();
+                RestartText.text = "Press 'N' for new game";
+                restart = true;
+                GameOver();
+                break;
+
+            }
+           
+        }
+
+    }
+
+    public void ResetBtn()
+    {
+        timer = mainTimer;
+        canCount = true;
+        doOnce = false;
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void GameOver ()
+    {
+        GameOverText.text = "GAME CREATED BY ALISON MUNNIS";
+        gameOver = true;
+        GetComponent<AudioSource>().Stop();
+        loseMusic.Play();
+
+        if (score >= 150)
+        {
+            loseMusic.Stop();
+            winMusic.Play();
+        }
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 150)
+        {
+            winText.text = "You win! GAME CREATED BY ALISON MUNNIS ";
+            //GetComponent<AudioSource>().Stop();
+            //loseMusic.Stop();
+            //winMusic.Play();
+            gameOver = true;
+            restart = true;
+        }
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public float pickupTime = 4f;
+
+    private Rigidbody rb;
+    private bool doOnce;
+    private bool active;
+    private float multiplier = 2f;
+
+
+    public GameObject shot;
+    public Transform[] shotSpawns;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            foreach (var shotSpawn in shotSpawns)
+            {
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            }
+                GetComponent<AudioSource>().Play ();
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        doOnce = true;
+        active = false;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+             0.0f,
+             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup") && doOnce == true && active == false)
+        {
+            Destroy(other.gameObject);
+            doOnce = false;
+            active = true;
+            StartCoroutine(Pickup());
+        }
+    }
+
+    IEnumerator Pickup()
+    {
+        speed *= multiplier;
+
+        yield return new WaitForSeconds(pickupTime);
+
+        speed /= multiplier;
+        doOnce = true;
+        active = false;
+
+    }
+
+}
